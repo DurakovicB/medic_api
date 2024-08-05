7df4d431fde0ac2317d923c48ce448868d9f35c0
Bilal Durakovic <bilal.durakovic@stu.ibu.edu.ba>
2024-08-05 04:20:12 +0200

    allowed cross origin requests

--- index.php
+++ index.php
@@ -1,4 +1,22 @@
 <?php
 
+// Allow from any origin
+header("Access-Control-Allow-Origin: *");
+
+// Allow specific HTTP methods
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+
+// Allow specific headers
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
+// Handle preflight requests
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    // If you are using cookies, you should also add 'Access-Control-Allow-Credentials' header
+    header("Access-Control-Allow-Credentials: true");
+    // Terminate the script if it's a preflight request
+    exit(0);
+}
+
+
 require 'vendor/autoload.php';
 include_once('routes.php');
